7c17fc36623e8ef22b1fb321aa0f14fdee83250d
unknown <Sandro-SSG>
2025-11-27 11:09:47 +0200

    Completed

--- Primes.java
+++ Primes.java
@@ -1,31 +1,39 @@
 public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
-        int q = Integer.parseInt(args());
+        int q = Integer.parseInt(args[0]);
         boolean[] num = new boolean[q + 1];
-        
-        System.out.println("Prime numbers up to " + (num.length -1) +  ":");
 
-        
-        for(int i = 2; i < num.length; i++){
-            num[i] = true;
-            if(i % 2 == 0 && i != 2){
-                num[i] = false;
-            }else if(i % 3 == 0 && i != 3){
-                num[i] = false;
-            }else if(i % 5 == 0 && i != 5){
-                num[i] = false;
-            }
+        int i = 2;
+        while (i <= q) {
+            num [i]= true;
+            i++;
         }
-        int a = 0;
-        for(int u = 2; u < num.length; u++){
-                if (num[u] == true){
-                   System.out.println(u); 
-                   a++;
+        int p = 2;
+        while ( p * p <= q){
+            if (num[p]){
+                int krat = p * p;
+                while ( krat <= q){
+                    num[krat] = false;
+                    krat = krat + p;
                 }
             }
-            int c = (a * 100) / (num.length - 1);
-            System.out.println("There are " + a + " primes between 2 and " + (num.length - 1) + " ("  + c + "% are primes)");
+            p++;
+        }
+        System.out.println("Prime numbers up to " + q + ":");
+        
+        int count = 0;
+        int k = 2;
+        while (k <= q) {
+            if (num[k]) {
+                System.out.println(k);
+                count++;
+            }
+            k++;
+        }
+
+            int c = (count * 100) / (num.length - 1);
+            System.out.println("There are " + count + " primes between 2 and " + (q) + " ("  + c + "% are primes)");
 
     }
 }
